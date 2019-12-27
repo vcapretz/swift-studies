00d4ec35857340b2e3ce3bce831cf12e814608d2
Vitor Capretz <capretzvitor@gmail.com>
2019-12-27 16:12:04 -0300

    GuessTheFlag: playable game complete

--- UIKit/Project2/Project2/ViewController.swift
+++ UIKit/Project2/Project2/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     var score = 0
     var countries = [String]()
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,20 +24,48 @@ class ViewController: UIViewController {
         
         button1.layer.borderColor = UIColor.lightGray.cgColor
         button1.layer.borderWidth = 1
+        button1.tag = 0
         
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button2.layer.borderWidth = 1
+        button2.tag = 1
         
         button3.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderWidth = 1
+        button3.tag = 2
         
         askQuestion()
     }
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
     }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct!"
+            score += 1
+        } else {
+            title = "Wrong..."
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+    }
+    
 }
 
