3b18bd1c3ca33f0d2800242648a20ab065709307
Vitor Capretz <capretzvitor@gmail.com>
2019-12-28 00:11:35 -0300

    StormViewer: add share button

--- UIKit/Project3/Project1/DetailViewController.swift
+++ UIKit/Project3/Project1/DetailViewController.swift
@@ -22,9 +22,9 @@ class DetailViewController: UIViewController {
             title = "Picture \(index) of \(count)"
         }
         
-//        title = selectedImage
         navigationItem.largeTitleDisplayMode = .never
-
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
             imageView.contentMode = .scaleAspectFill
@@ -39,5 +39,17 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 1) else {
+            print("Failed to load image...")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        
+        present(vc, animated: true)
     }
 }
