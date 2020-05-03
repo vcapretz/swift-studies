4d50eb1470959f80ee0c76f2b87461f7d13b77bf
Vitor Capretz <capretzvitor@gmail.com>
2020-05-03 20:41:28 +0200

    first commit

---
+++ Udemy/I am poor/I am poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Vitor Capretz on 2020-02-29.
+//  Copyright © 2020 Vitor Capretz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
